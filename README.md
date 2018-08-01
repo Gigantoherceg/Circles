e62a6e1e9fea20280dcee3b29b8ae8df721edeb7
Gigantoherceg <mgaboor@gmail.com>
2018-08-01 17:05:32 +0200

    Random circles in the canvas

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Circles
+# Project Title
+
+It is a simple graphic from some circles.
+Push the Reload (F5) button.
+
+## Built With
+
+* HTML5, CSS, JavaScript - The languages
+* [JS Bin](https://jsbin.com/) - The online editor
---
+++ scripts.js
@@ -0,0 +1,25 @@
+var canvas = document.getElementById('Cilrcles');
+var context = canvas.getContext('2d');
+
+var cW = canvas.width;
+var cH = canvas.height;
+var x, y, r, g, b, a, j = 1;
+var radius = Math.floor(Math.random() * 100)+10;
+var circleNumber = Math.floor(Math.random() * 100)+1;
+
+function circle(x,y, radius, color) {  
+  context.strokeStyle = color;
+  context.beginPath();
+  context.arc(x,y,radius,0,2*Math.PI);
+  context.stroke();
+}
+
+for (j = 1; j <= circleNumber; j++) {
+  x = Math.floor(Math.random() * (cW-2*radius)) + radius;
+  y = Math.floor(Math.random() * (cH-2*radius)) + radius;
+  r = Math.floor(Math.random() * 255);
+  g = Math.floor(Math.random() * 255);
+  b = Math.floor(Math.random() * 255);
+  a = (Math.floor(Math.random() * 10) + 1)/10;
+  circle(x,y,radius,'rgba('+r+','+g+','+b+','+a+')');  
+}
